199840d23e02c273def949ae970ee0e843e8fddc
Paul Denlinger <pmdenlinger@icloud.com>
2021-05-16 09:03:37 -0700

    Initial Commit

---
+++ pizza-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pizza-challenge
+//
+//  Created by user on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pizza-challenge/pizza_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pizza_challengeApp.swift
+//  pizza-challenge
+//
+//  Created by user on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct pizza_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
